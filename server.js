3d8ba66d0980e57b3138c48a00dab2eea334b906
Amine Ben Faiza <amine.benfaiza@esprit.tn>
2024-06-01 14:20:47 +0100

    fixed server and package-lock.js errors

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
-
 import express from 'express';
 import mongoose from 'mongoose'; 
 import dotenv from 'dotenv';
 import userRoutes from './routes/user.js';
+import eventRoutes from './routes/event.js';
+import eventCategory from './routes/eventCategory.js';
+import mapRoutes  from'./routes/geocode.js';
+import paymentRoutes  from'./routes/payment.js';
+import publicationRoutes from "./routes/publication.routes.js";
+import commentRoutes from "./routes/comment.route.js";
 
 dotenv.config();
 
@@ -25,67 +30,15 @@ mongoose
 app.use(express.json());
 
 app.use('/user', userRoutes);
-
-app.listen(port, () => {
-  console.log(`Server running at http://127.0.0.1:${port}/`);
-});
-
-import express from 'express';
-import mongoose from 'mongoose';
-
-import userRoutes from './routes/user.js';
-import eventRoutes from './routes/event.js';
-import eventCategory from './routes/eventCategory.js';
-import mapRoutes  from'./routes/geocode.js';
-import paymentRoutes  from'./routes/payment.js';
-
-
-=======
-import userRoutes from "./routes/user.js";
-import publicationRoutes from "./routes/publication.routes.js";
-import commentRoutes from "./routes/comment.route.js";
-//importing models
-import User from "./models/user.js";
-import Publication from "./models/publication.js";
-import comment from "./models/comment.js";
-
-import dotenv from 'dotenv';
-
-dotenv.config();
-
-const app = express();
-const port = process.env.PORT || 8080;
-const databaseName = "ThinkPro";
-
-// Cela afichera les requêtes MongoDB dans le terminal
-mongoose.set("debug", true);
-// Utilisation des promesses ES6 pour Mongoose, donc aucune callback n'est nécessaire
-mongoose.Promise = global.Promise;
-
-mongoose
-  .connect(`mongodb://127.0.0.1:27017/${databaseName}`)
-  .then(() => {
-    console.log(`Connected to ${databaseName}`);
-  })
-  .catch((err) => {
-    // Si quelque chose ne va pas, afficher l'erreur sur la console
-    console.log(err);
-  });
-
-app.use(express.json());
-
-app.use('/user', userRoutes);
 app.use('/event', eventRoutes);
 app.use('/eventCategory', eventCategory);
 app.use('/payment', paymentRoutes);
 app.use('/map', mapRoutes);
+app.use("/", publicationRoutes);
+app.use("/", commentRoutes);
+
+
 
 app.listen(port, () => {
   console.log(`Server running at http://127.0.0.1:${port}/`);
 });
-
-
-app.use("/user", userRoutes);
-app.use("/", publicationRoutes);
-app.use("/", commentRoutes);
-
