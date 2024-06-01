96141803d336b163d85ace8be3c44af5dffbb5cd
nourbayoudh <94978029+nourbayoudh@users.noreply.github.com>
2024-06-01 14:46:59 +0200

    Inscription

--- controllers/user.js
+++ controllers/user.js
@@ -1,50 +1,9 @@
 import User from '../models/user.js';
-import bcrypt from 'bcrypt';
-import { generateToken } from '../jwt.js';
-import nodemailer from 'nodemailer';
-import crypto from 'crypto';
-import 'dotenv/config';
+
+const users=[];
 
 
-
-
-export async function signup(req, res) {
-    try {
-        const { userName,passWord,email,adress,phone,role,dateOfB } = req.body;
-        const existingUser = await User.findOne({ email });
-        if (existingUser) {
-            return res.status(400).json({ message: "L'utilisateur existe déjà" });
-        }
-        const hashedPassword = await bcrypt.hash(passWord, 10);
-        const newUser = await User.create({ userName, passWord: hashedPassword ,email,adress,phone,role,dateOfB });
-        res.status(201).json({ message: "Inscription réussie", user: newUser });
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ error: "Une erreur s'est produite lors de l'inscription" });
-    }
-}
-export async function signin(req, res) {
-    try {
-        const { email, passWord } = req.body;
-        const user = await User.findOne({ email });
-        if (!user) {
-            return res.status(404).json({ message: "L'utilisateur n'existe pas" });
-        }
-        const passwordMatch = await bcrypt.compare(passWord, user.passWord);
-        if (!passwordMatch) {
-            return res.status(401).json({ message: "Mot de passe incorrect" });
-        }
-        
-                const authToken = generateToken({ userId: user._id, email: user.email });
-                const expireToken = Date.now() + 3600000;
-
-        res.status(200).json({ message: "Connexion réussie", authToken });
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ error: "Une erreur s'est produite lors de la connexion" });
-    }
-}
-export function getAllUsers(req, res) {
+export function getAll(req, res) {
     User
     .find({})
     .then(docs => {
@@ -54,18 +13,15 @@ export function getAllUsers(req, res) {
         res.status(500).json({ error: err });
     });
 }
-export async function AddUser(req, res) {
+export async function AddOnce(req, res) {
     const user = new User({
         userName: req.body.userName,
-        passWord: req.body.passWord = await bcrypt.hash(req.body.passWord, 10),
+        passWord: req.body.passWord,
         email: req.body.email,
         adress: req.body.adress,
         phone: req.body.phone,
-        role: req.body.role,
-        dateOfB:req.body.dateOfB
+        role: req.body.role
     });
-         
-
 
     try {
         const newUser = await User.create(user);
@@ -80,9 +36,11 @@ export async function AddUser(req, res) {
     }
 }
 
-export async function getUserByEmail(req, res) {
+
+
+export async function getOnce(req, res) {
     try {
-        const user = await User.findOne({ email: req.params.email });
+        const user = await User.findOne({ userName: req.params.userName });
         
         if (!user) {
             return res.status(404).json({ message: "Utilisateur non trouvé" });
@@ -94,32 +52,14 @@ export async function getUserByEmail(req, res) {
         res.status(500).json({ error: "Erreur lors de la recherche de l'utilisateur" });
     }
 }
-// export async function getUserByName(req, res) {
-//     try {
-//         const user = await User.findOne({ userName: req.params.userName });
-        
-//         if (!user) {
-//             return res.status(404).json({ message: "Utilisateur non trouvé" });
-//         }
-        
-//         res.status(200).json(user);
-//     } catch (error) {
-//         console.error(error);
-//         res.status(500).json({ error: "Erreur lors de la recherche de l'utilisateur" });
-//     }
-// }
 
-export async function putUser(req, res) {
+
+
+export async function putOnce(req, res) {
     try {
-
-        if (req.body.passWord) {
-            req.body.passWord = await bcrypt.hash(req.body.passWord, 10);
-        }
-
-
         const updatedUser = await User.findOneAndUpdate(
-            {  _id:req.params.userId}, 
-            { userName: req.body.userName,passWord: req.body.passWord, email: req.body.email, adress: req.body.adress , phone: req.body.phone, role: req.body.role,dateOfB:req.body.dateOfB }, // Données à mettre à jour
+            { userName: req.params.userName }, 
+            { passWord: req.body.passWord, email: req.body.email, adress: req.body.adress , phone: req.body.phone, role: req.body.role }, // Données à mettre à jour
             { new: true } 
         );
 
@@ -127,7 +67,7 @@ export async function putUser(req, res) {
             return res.status(404).json({ message: "Utilisateur non trouvé" });
         }
 
-        res.status(201).json({
+        res.status(200).json({
             message: "Utilisateur mis à jour avec succès !",
             user: updatedUser
         });
@@ -137,15 +77,11 @@ export async function putUser(req, res) {
     }
 }
 
-export async function patchUser(req, res) {
+
+export async function patchOnce(req, res) {
     try {
-
-        if (req.body.passWord) {
-            req.body.passWord = await bcrypt.hash(req.body.passWord, 10);
-        }
-
         const updatedUser = await User.findOneAndUpdate(
-            { _id: req.params.userId }, 
+            { userName: req.params.userName }, 
              { $set: req.body },
             { new: true } 
         );
@@ -164,9 +100,10 @@ export async function patchUser(req, res) {
     }
 }
 
-export async function deleteUser(req, res) {
+
+export async function deleteOnce(req, res) {
     try {
-        const deletedUser = await User.findOneAndDelete({ email: req.params.email });
+        const deletedUser = await User.findOneAndDelete({ userName: req.params.userName });
 
         if (!deletedUser) {
             return res.status(404).json({ message: "Utilisateur non trouvé" });
@@ -181,76 +118,3 @@ export async function deleteUser(req, res) {
         res.status(500).json({ error: "Erreur lors de la suppression de l'utilisateur" });
     }
 }
-
-export const forgotPassword = async (req, res) => {
-    try {
-        const { email } = req.body;
-        const user = await User.findOne({ email });
-        if (!user) {
-            return res.status(404).send({ error: 'Utilisateur non trouvé' });
-        }
-
-        const token = crypto.randomBytes(20).toString('hex');
-        let date = new Date();
-        const tokenExpiry = date.setHours(date.getHours() + 1);
-        
-
-
-        console.log(token);
-        user.resetPasswordToken = token;
-        user.resetPasswordExpires = tokenExpiry;
-        await user.save();
-
-        const transporter = nodemailer.createTransport({
-            service: 'Gmail',
-            auth: {
-                user: process.env.EMAIL_USER,
-                pass: process.env.EMAIL_PASS,
-            },
-        });
-
-        const mailOptions = {
-            to: user.email,
-            from: process.env.EMAIL_USER,
-            subject: 'Réinitialisation de mot de passe',
-            text: `Vous recevez cet email parce que vous (ou quelqu'un d'autre) avez demandé la réinitialisation du mot de passe de votre compte.\n\n
-                   Veuillez cliquer sur le lien suivant, ou le copier dans votre navigateur pour compléter le processus dans l'heure suivant la réception de cet email:\n\n
-                   http://${req.headers.host}/reset-password/${token}\n\n
-                   Si vous n'avez pas demandé cette réinitialisation, veuillez ignorer cet email et votre mot de passe restera inchangé.\n`
-        };
-
-        await transporter.sendMail(mailOptions);
-
-        res.status(200).send({ message: 'Email de réinitialisation envoyé' });
-    } catch (error) {
-        res.status(500).send({ error: error.message });
-        
-    }
-};
-
-export const resetPassword = async (req, res) => {
-    try {
-        const { token } = req.params;
-        const { password } = req.body;
-
-        const user = await User.findOne({
-            resetPasswordToken: token,
-            resetPasswordExpires: { $gt: Date.now() }
-        });
-
-        if (!user) {
-            return res.status(400).send({ error: 'Token de réinitialisation invalide ou expiré' });
-        }
-        const saltRounds = 10;
-        const hashedPassword = await bcrypt.hash(user.passWord, saltRounds);
-
-        user.resetPasswordToken = undefined;
-        user.resetPasswordExpires = undefined;
-        user.passWord=hashedPassword;
-        await user.save();
-
-        res.status(200).send({ message: 'Mot de passe réinitialisé avec succès' });
-    } catch (error) {
-        res.status(500).send({ error: error.message });
-    }
-};
--- server.js
+++ server.js
@@ -1,58 +1,47 @@
-
 import express from 'express';
-import mongoose from 'mongoose'; 
-import dotenv from 'dotenv';
+import mongoose from 'mongoose'; // Importer Mongoose
+import nodemailer from 'nodemailer';
 import userRoutes from './routes/user.js';
-
-import produitRoutes from './routes/Produit.js';
-import typeRoutes from './routes/typeProduit.js';
-import commandeRoutes from './routes/commande.js';
-import publicationRoutes from "./routes/publication.routes.js";
-import commentRoutes from "./routes/comment.route.js";
-import User from "./models/user.js";
-import Publication from "./models/publication.js";
-import comment from "./models/comment.js";
-
-dotenv.config();
-app.use(express.json());
+import inscriptionRoutes from './routes/inscription.js'
+import offreRoutes from './routes/offre.js'
+import paymentRoutes  from'./routes/payment.js';
+import Type_offreRoutes  from'./routes/Type_offre.js';
 
 
-app.use('/user', userRoutes);
-app.use('/produit', produitRoutes);
-app.use('/type', typeRoutes);
-app.use('/commande', commandeRoutes);
-app.use("/", publicationRoutes);
-app.use("/", commentRoutes);
 
-mongoose.set('debug', true);
-mongoose.Promise = global.Promise;
 const app = express();
 const port = process.env.PORT || 8080;
-const databaseName = "ThinkPro";
+const databaseName = 'ThinkPro';
+
+// Cela afichera les requêtes MongoDB dans le terminal
+mongoose.set('debug', true);
+// Utilisation des promesses ES6 pour Mongoose, donc aucune callback n'est nécessaire
+mongoose.Promise = global.Promise;
+
+// Se connecter à MongoDB
 mongoose
   .connect(`mongodb://127.0.0.1:27017/${databaseName}`)
   .then(() => {
+    // Une fois connecté, afficher un message de réussite sur la console
     console.log(`Connected to ${databaseName}`);
   })
   .catch(err => {
+    // Si quelque chose ne va pas, afficher l'erreur sur la console
     console.log(err);
   });
 
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use('/img', express.static('public/images'));
+
+app.use('/user', userRoutes);
+app.use('/inscription', inscriptionRoutes);
+app.use('/offre', offreRoutes);
+app.use('/payment', paymentRoutes);
+app.use('/Type_offre', Type_offreRoutes);
+
+
 
 app.listen(port, () => {
   console.log(`Server running at http://127.0.0.1:${port}/`);
 });
-
-
-console.log('Starting server...');
-console.log('Connecting to MongoDB...');
-
-
-
-app.listen(port, () => {
-  console.log(`Server running at http://127.0.0.1:${port}/`);
-});
-
-
-
-
